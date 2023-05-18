87ec14ede7a2c86909c5aa26342773ce88f16840
Pedro Silva <Pedrosilva_02@outlook.com>
2023-05-18 19:58:27 +0100

    Safe Map Improvement

--- include/pl/safe_map.hpp
+++ include/pl/safe_map.hpp
@@ -8,37 +8,42 @@
 
 namespace pl {
 
+    template<typename T, typename C> class locked_map {
+    private:
+        std::shared_ptr<std::mutex> m_mtx;
+        std::shared_ptr<std::map<T, C>> m_map;
+    public:
+        locked_map(const std::shared_ptr<std::mutex>& _mtx, const std::shared_ptr<std::map<T, C>>& _map) : m_mtx(_mtx), m_map(_map) {
+            m_mtx->lock();
+        }
+
+        ~locked_map() {
+            m_mtx->unlock();
+        }
+
+        std::map<T, C>* get() const {
+            return m_map.get();
+        }
+
+        std::map<T, C>* operator->() {
+            return m_map.get();
+        }
+    };
+
     /***
      * map wrapper to guarantee thread safety
      * @tparam T key type
      * @tparam C get type
      */
-    template<typename T, typename C> class safe_map : public std::map<T, C> {
+    template<typename T, typename C> class safe_map {
     private:
         std::shared_ptr<std::mutex> m_mtx;
+        std::shared_ptr<std::map<T, C>> m_map;
     public:
-        safe_map() : m_mtx(std::make_shared<std::mutex>()) { };
+        safe_map() : m_mtx(std::make_shared<std::mutex>()), m_map(std::make_shared<std::map<T, C>>()) { };
 
-        inline bool safe_contains(const T& key) {
-            lock();
-            auto result = this->contains(key);
-            unlock();
-            return result;
-        }
-
-        inline void lock() {
-            m_mtx->lock();
-        }
-
-        inline void unlock() {
-            m_mtx->unlock();
-        }
-
-        inline bool is_locked() {
-            auto result = m_mtx->try_lock();
-            if (result)
-                m_mtx->unlock();
-            return result;
+        [[nodiscard]] inline locked_map<T, C> lock() const {
+            return locked_map<T, C>(m_mtx, m_map);
         }
     };
 }
